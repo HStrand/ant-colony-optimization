8f5285abcbd519b3ae05887f4064c16b8c91020a
HStrand <hhstrand@gmail.com>
2016-09-05 23:56:18 +0200

    Added Travelling Salesman Problem

--- aco.py
+++ aco.py
@@ -1,17 +1,52 @@
 import numpy as np
+import pandas as pd
 import time
 
-MATRIX = np.array([[0,0.4,0.3,0.2,0.1], [0.1,0,0.4,0.3,0.2], [0.1,0.2,0,0.3,0.4], [0.1,0.3,0.2,0,0.4], [0.3,0.4,0.2,0.1,0]])
+
+COLONY_SIZE = 30
+ALPHA = 1
+BETA = 5
+EVAPORATION_RATE = 0.5
+Q = 500
+INITIAL_PHEROMONE = 1
+P_RANDOM_CELL = 0.01
+
+distances = pd.read_csv('distances.csv', sep=';')
+numerated = {}
+cities = distances.columns.values
+for i in range(1,len(cities)):
+    numerated[i] = cities[i]
+
+P = np.array([[INITIAL_PHEROMONE]*len(distances)]*len(distances)) # Initial transition matrix
+inverse_identity = 1 - np.identity(8) # Inverse identity matrix
+P = P*inverse_identity # Set diagonal to 0 to avoid transitionss to current state
+
+def get_row(city):
+	return np.array(distances)[numerated[city],1:]
+
+def get_row_by_num(num):
+	return np.array(distances)[num,1:]
+
+def get_column(city):
+	return np.array(distances[city])
+
+def get_distance(a, b):
+	return get_row_by_num(a)[b]
+
 
 class Ant:
-	def __init__(self, trail):
+	def __init__(self, trail, initial_location):
 		self.path = []
-		self.location = 0
-		self.matrix = MATRIX 
+		self.initial_location = initial_location
+		self.location = initial_location		
+		self.distance_travelled = 0
+		self.matrix = P 
 		""" 
 		The ant keeps a copy of the transition matrix. 
 		This is also used to keep track of the places the ant has visited. 
+		THe initial location has to be the last stop.
 		"""
+		self.matrix[:,self.location]=0
 
 	def step(self):
 		transition_probabilities = self.matrix[self.location]
@@ -27,19 +62,32 @@ class Ant:
 				step = i
 				break
 		
-		print("Step to", step)
+		distance = get_distance(self.location, step) # Record distance travelled
+		print("Travelling", distance, "km from", numerated[self.location+1], "to", numerated[step+1])
+
 		self.location = step # Move ant to the new location.
 		self.matrix[:,step] = 0 # Ensure ant will not visit this place again.
+		self.path.append(step)
+		self.distance_travelled += distance
+
 
 	def walk(self):
-		for step in range(0,len(MATRIX)):
+		for step in range(0,len(P)-1): # -1 since initial location is not available.
 			self.step()
+
+		distance = get_distance(self.location, self.initial_location)
+		print("Travelling", distance, "km from", numerated[self.location+1], "to", numerated[self.initial_location+1])
+		self.path.append(self.initial_location)
+		self.distance_travelled += distance
+		self.location = self.initial_location
+		print("Path:", self.path)
+		print("Distance travelled:", self.distance_travelled, "km")
 
 
 if __name__ == '__main__':
 	start = time.time()
-	ant = Ant(None)
+	ant = Ant(None, 0)
 	ant.walk()
 	#for i in range(0,len(MATRIX)):
 	#	ant.step()
-	print(time.time()-start)
+	print("Time:", time.time()-start)
